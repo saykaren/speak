4f43b56ecefa9a2e1b102319b9c8e7ef52b802e5
Karen Garcia <saykaren@gmail.com>
2022-08-27 09:24:53 -0600

    Adds filter

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,48 @@
-import '../Styling/App.css';
+import "../Styling/App.css";
+import React, { useState } from "react";
+import Data from "../Data/data";
 
-function App() {
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-here
-      </header>
-    </div>
+    <>
+      <h1>Speak Another Language</h1>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search Page"
+        id="inputSearchTerm"
+      />
+
+      <section>
+        <table>
+          <tr>
+            <th className="tableColumn">English Word</th>
+            <th className="tableColumn">French Spelling</th>
+            <th className="tableColumn">French Pronunciation</th>
+            <th className="tableColumn">Spanish Spelling</th>
+            <th className="tableColumn">Mandarin Pronunciation</th>
+          </tr>
+
+          {Data &&
+            Data.filter((item) =>
+              item.englishWord
+                .toLocaleLowerCase()
+                .includes(searchTerm.toLocaleLowerCase())
+            ).map((x, xKey) => (
+              <tr key={xKey}>
+                <td className="tableColumn">{x.englishWord}</td>
+                <td className="tableColumn">{x.frenchSpelling}</td>
+                <td className="tableColumn">{x.frenchPronunciation}</td>
+                <td className="tableColumn">{x.spanishSpelling}</td>
+                <td className="tableColumn">{x.mandarianPronunciation}</td>
+              </tr>
+            ))}
+        </table>
+      </section>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Data/data.js
@@ -0,0 +1,25 @@
+export const languageData = [
+  {
+    englishWord: "I",
+    frenchSpelling: "je",
+    frenchPronunciation: "jet with no t",
+    spanishSpelling: "Yo",
+    mandarianPronunciation: "Wah"
+  },
+  {
+    englishWord: "You",
+    frenchSpelling: "Vous",
+    frenchPronunciation: "voo",
+    spanishSpelling: "tu",
+    mandarianPronunciation: ""
+  },
+//   {
+//     englishWord: "I",
+//     frenchSpelling: "je",
+//     frenchPronunciation: "jet with no t",
+//     spanishSpelling: "Yo",
+//     mandarianPronunciation: "Wah"
+//   },
+];
+
+export default languageData;
