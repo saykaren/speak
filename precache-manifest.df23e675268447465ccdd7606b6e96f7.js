c87b748718af0dd3b97a489aa09b5e2cb5f62ff2
Karen Garcia <saykaren@gmail.com>
2022-08-27 09:45:46 -0600

    Updates

---
+++ precache-manifest.df23e675268447465ccdd7606b6e96f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc3cb355c18c13a6d01",
+    "url": "/speak/static/css/main.a5f200dc.chunk.css"
+  },
+  {
+    "revision": "5cc3cb355c18c13a6d01",
+    "url": "/speak/static/js/main.5cc3cb35.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/speak/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "e30cd94f1be928d6505d",
+    "url": "/speak/static/js/2.e30cd94f.chunk.js"
+  },
+  {
+    "revision": "0b816d0f68ccedc5662a",
+    "url": "/speak/static/js/runtime~main.0b816d0f.js"
+  },
+  {
+    "revision": "d0827d01ac17b128172a9f867b8d0c82",
+    "url": "/speak/static/media/sayKarenWhite.d0827d01.png"
+  },
+  {
+    "revision": "f0c45a32f5d3f7cee048132c6cee74d7",
+    "url": "/speak/index.html"
+  }
+];
