67685b036815ff08fb7c2747151be5eca89767cc
Karen Garcia <saykaren@gmail.com>
2022-08-27 09:02:18 -0600

    First commit

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import '../Styling/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+here
+      </header>
+    </div>
+  );
+}
+
+export default App;
